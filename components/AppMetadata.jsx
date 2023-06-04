9880c1ea78efdc4734230bb7823f9afe282262db
Bharat Dussa <bharatdussa@Bharats-MacBook-Pro.local>
2023-06-04 20:19:53 +0530

    change metatdaata

--- components/AppMetadata.jsx
+++ components/AppMetadata.jsx
@@ -1,9 +1,9 @@
-const author = "Vasile Novatchii";
+const author = "Bharat Dussa";
 const description =
-	"Software developer from Moldova, Republic of, who loves to develop beautiful websites, single page applications, customer relationship management or code from scratch using React and Next.js";
-const url = "https://vasile-novatchii.netlify.app";
+	"";
+const url = "https://bharat-dussa.vercel.app/";
 export const AppMetadata = {
-	metadataBase: new URL("https://vasile-novatchii.netlify.app/"),
+	metadataBase: new URL("https://bharat-dussa.vercel.app/"),
 	title: {
 		default: `Portfolio | ${author}`,
 		template: `%s | ${author}`
@@ -13,10 +13,10 @@ export const AppMetadata = {
 		icon: "/favicon.png"
 	},
 	keywords: [
-		"Vasile Novatchii",
-		"Vasile Novatchii - software developer",
+		"Bharat Dussa",
+		"Bharat Dussa - software developer",
 		"Frontend developer",
-		"Flutter developer",
+		"React developer",
 		"Portfolio website",
 		"Frontend Developer Portfolio"
 	],
@@ -30,13 +30,13 @@ export const AppMetadata = {
 		siteName: `${author} | Portfolio`,
 		images: [
 			{
-				url: "https://vasile-novatchii.netlify.app/screenshot.webp",
+				url: "",
 				width: 800,
 				height: 600,
 				alt: "My personal portfolio website"
 			},
 			{
-				url: "https://vasile-novatchii.netlify.app/screenshot.webp",
+				url: "",
 				width: 1800,
 				height: 1600,
 				alt: "My personal portfolio website"
