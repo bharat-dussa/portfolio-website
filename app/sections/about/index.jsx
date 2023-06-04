26d7686bc72407415dade62395a9155990ad0f1a
Bharat Dussa <bharatdussa@Bharats-MacBook-Pro.local>
2023-06-04 20:17:39 +0530

    add color to text

--- app/sections/about/index.jsx
+++ app/sections/about/index.jsx
@@ -25,24 +25,32 @@ export function AboutSection() {
 						}}
 					>
 						<p>
-							My name is Bharat Dussa. I successfully graduated from University of Mumbai in
-							bachelors of Computers
-						</p>
-						<p>
-							During my university years, I developed a kneen interest for programming and networking, and I
-							consistently engaged in learning new concepts and techniques in this field.
-						</p>
-						
-						<p className="my-3.5">
-							I enjoy learning technologies that interest me
-						</p>
-						<p>
-							I started learning React. I was so excited to see how fast I can build a website with it, and how easy it is to maintain
-							it. Consequently, I do freelancing for possible candidates
+							My name is Bharat Dussa, and I successfully graduated from the University of Mumbai
+							with a<span className="text-orange-300"> bachelor&apos;s degree in Computers.</span>
 						</p>
 						<p className="my-3.5">
-							I started my React journey from Nov 2020 after graduation, and learn new things while working.
+							During my university years, I developed a keen interest in programming and networking,
+							constantly seeking opportunities to learn new concepts and techniques in the field.
 						</p>
+						<p className="my-3.5">
+							Since graduating, I have dedicated myself to expanding my skills and knowledge in
+							React, continuously learning and staying up to date with the latest advancements in
+							the framework.
+						</p>
+						<p className="my-3.5">
+							I am passionate about applying my expertise to develop innovative web solutions and
+							contribute to projects that push the boundaries of web development.
+						</p>
+						<p>
+							As part of my learning journey, I discovered React, a powerful JavaScript library for
+							building user interfaces. I was instantly captivated by its speed and efficiency in
+							website development, as well as its ease of maintenance. Motivated by this, I delved
+							into React and embarked on a freelancing journey, assisting clients in leveraging
+							<span className="text-orange-300"> React</span> to create modern and dynamic websites.
+							<span className="text-orange-300"> Node</span> for backend, and some stuff in{" "}
+							<span className="text-orange-300"> NextJs</span>
+						</p>
+						<p className="my-3.5">I enjoy learning technologies that interest me</p>
 					</div>
 				</div>
 
